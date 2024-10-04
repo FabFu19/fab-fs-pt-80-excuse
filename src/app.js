0180e958eda34d40e2812611b7b7389dd041652c
FabFu19 <fabianfuen19@icloud.com>
2024-10-04 19:41:33 +0000

    feat: delivery excuse generator created

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const excuseGene = document.querySelector("#excuses4geeks");
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  return (excuseGene.innerHTML = generatingExcuses());
+};
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+const getRandomExcuses = Array => Math.floor(Math.random() * Array.length);
+const getRandomValue = Array => Array[getRandomExcuses(Array)];
+const generatingExcuses = () => {
+  return `${getRandomValue(who)} ${getRandomValue(action)} ${getRandomValue(
+    what
+  )} ${getRandomValue(when)}`;
 };
