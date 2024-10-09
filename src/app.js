53a66be31bf81df3532392493c21b7cc4feb3bef
FabFu19 <fabianfuen19@icloud.com>
2024-10-09 13:23:56 +0000

    feat(fixing the array name): added background change each time page reloeded

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ import "./assets/img/4geeks.ico";
 
 const excuseGene = document.querySelector("#excuses4geeks");
 window.onload = function() {
-  return (excuseGene.innerHTML = generatingExcuses());
+  excuseGene.innerHTML = generatingExcuses();
+  getChangeColorQuote();
 };
 
 let who = ["The dog", "My grandma", "The mailman", "My bird"];
@@ -19,9 +20,18 @@ let when = [
   "during my lunch",
   "while I was praying"
 ];
+let bgColorQuote = ["#ff82ba", "#db8741", "#145c82", "#2099d8", "#75407b"];
 
-const getRandomExcuses = Array => Math.floor(Math.random() * Array.length);
-const getRandomValue = Array => Array[getRandomExcuses(Array)];
+const getRandomExcuses = excuse_array =>
+  Math.floor(Math.random() * excuse_array.length);
+const getRandomValue = excuse_array =>
+  excuse_array[getRandomExcuses(excuse_array)];
+const getChangeColorQuote = () => {
+  const bgExcuse = document.querySelector(".excusecontent");
+  let randomColor = bgColorQuote[getRandomExcuses(bgColorQuote)];
+  bgExcuse.style.backgroundColor = randomColor;
+};
+
 const generatingExcuses = () => {
   return `${getRandomValue(who)} ${getRandomValue(action)} ${getRandomValue(
     what
